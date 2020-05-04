b7a26024b01620344b01559328f71a743deae966
darylbg <44724776+darylbg@users.noreply.github.com>
2020-05-04 14:40:07 +0100

    removed env.py

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault('STRIPE_PUBLISHABLE', 'pk_test_doIfqdnODmzgg00kfQcj9wHj00ld9K3l0D')
+os.environ.setdefault('STRIPE_SECRET', 'sk_test_i2pgDkCALtlRNslBmMFJ251w00PRqS8W0p')
+os.environ.setdefault('DATABASE_URL', 'postgres://anyxlyqkziephf:c44de24d8756dae4bf6ffac566474aff9d01d57068c283d5665c8fd6dc862a76@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/df44p941co06nd')
+os.environ.setdefault('SECRET_KEY', '1v^h##*6vhqr)1&%nx*6rsa$trk&$faojc&p6x5firpoon+*z*')
